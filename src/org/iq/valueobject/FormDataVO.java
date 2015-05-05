e4af1eb725423bff66781cda73ebe9e2a71e9bc9
iqpranith <pranith@iquesters.com>
2015-05-05 23:14:58 +0530

    #1 - Create Form Templates using Java Emitter Template

---
+++ src/org/iq/valueobject/FormDataVO.java
@@ -0,0 +1,40 @@
+package org.iq.valueobject;
+
+/**
+ * @author Sam
+ *
+ */
+public class FormDataVO extends BaseVO {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 955678878049885571L;
+	
+	/**
+	 * 
+	 */
+	private int formID = 0;
+	
+	/**
+	 * @return
+	 */
+	public int getFormID() {
+		return formID;
+	}
+
+	/**
+	 * @param formID
+	 */
+	public void setFormID(int formID) {
+		this.formID = formID;
+	}
+
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
