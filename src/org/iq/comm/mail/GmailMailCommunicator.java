b55e5b4b440b749fb7a5d54e8007e27c444439b6
Sambuddha Chatterjee <samhere17@gmail.com>
2016-01-20 21:54:40 +0530

    Modify the build process

--- src/org/iq/comm/mail/GmailMailCommunicator.java
+++ src/org/iq/comm/mail/GmailMailCommunicator.java
@@ -23,6 +23,7 @@ import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 
+import org.iq.SystemContext;
 import org.iq.comm.CommunicationManager.CommunicationType;
 import org.iq.util.StringUtil;
 
@@ -32,153 +33,153 @@ import org.iq.util.StringUtil;
  */
 public class GmailMailCommunicator extends MailCommunicator {
 
-  private static String username = "SystemConf.getMailUsername()";
-  private static String password = "SystemConf.getMailPassword()";
-  private static String smtpAuthFlag = "SystemConf.getMailAuthFlag()";
-  private static String smtpTLSEnableFlag = "SystemConf.getMailTLSEnableFlag()";
-  private static String smtpHost = "SystemConf.getMailHost()";
-  private static String smtpPort = "SystemConf.getMailPort()";
+	private static String username = SystemContext.systemConfig.getMailUsername();
+	private static String password = SystemContext.systemConfig.getMailPassword();
+	private static String smtpAuthFlag = SystemContext.systemConfig.getMailSmtpAuthFlag();
+	private static String smtpTLSEnableFlag = SystemContext.systemConfig.getMailSmtpTLSEnableFlag();
+	private static String smtpHost = SystemContext.systemConfig.getMailSmtpHost();
+	private static String smtpPort = SystemContext.systemConfig.getMailSmtpPort();
 
-  /**
-   * @param commType
-   */
-  public GmailMailCommunicator() {
-	super(CommunicationType.GMAIL_MAIL);
-  }
-
-  /*
-   * (non-Javadoc)
-   * 
-   * @see org.iq.comm.Communicator#validateReceipient()
-   */
-  @Override
-  protected boolean validateReceipient() {
-	boolean valid = false;
-	if (StringUtil.isEmpty(to) == false) {
-	  if (to.contains(",")) {
-		String[] emails = to.split(",");
-		valid = validateEmails(emails);
-	  }
-	  else {
-		valid = validateEmails(to);
-	  }
+	/**
+	 * @param commType
+	 */
+	public GmailMailCommunicator() {
+		super(CommunicationType.GMAIL_MAIL);
 	}
 
-	if (StringUtil.isEmpty(cc) == false) {
-	  if (cc.contains(",")) {
-		String[] emails = cc.split(",");
-		valid &= validateEmails(emails);
-	  }
-	  else {
-		valid &= validateEmails(cc);
-	  }
-	}
-
-	if (StringUtil.isEmpty(bcc) == false) {
-	  if (bcc.contains(",")) {
-		String[] emails = bcc.split(",");
-		valid &= validateEmails(emails);
-	  }
-	  else {
-		valid &= validateEmails(bcc);
-	  }
-	}
-
-	return valid;
-  }
-
-  /*
-   * (non-Javadoc)
-   * 
-   * @see org.iq.comm.Communicator#processCommunication(java.lang.String,
-   * java.lang.String, java.lang.String, java.lang.String, java.lang.String,
-   * java.lang.String, java.lang.String)
-   */
-  @Override
-  protected boolean processCommunication(String from, String to, String cc,
-	  String bcc, String subject, String message, String attachmentFilename) {
-
-	if (StringUtil.isEmpty(username) == false
-		&& StringUtil.isEmpty(password) == false) {
-	  Properties props = new Properties();
-	  props.put(SMTP_AUTH_PARAM_NAME, smtpAuthFlag);
-	  props.put(SMTP_TLS_ENABLE_PARAM_NAME, smtpTLSEnableFlag);
-	  props.put(SMTP_HOST_PARAM_NAME, smtpHost);
-	  props.put(SMTP_PORT_PARAM_NAME, smtpPort);
-
-	  Session session = Session.getInstance(props,
-		  new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-			  return new PasswordAuthentication(userName, password);
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.iq.comm.Communicator#validateReceipient()
+	 */
+	@Override
+	protected boolean validateReceipient() {
+		boolean valid = false;
+		if (StringUtil.isEmpty(to) == false) {
+			if (to.contains(",")) {
+				String[] emails = to.split(",");
+				valid = validateEmails(emails);
 			}
-		  });
-
-	  if (/*StringUtil.isEmpty(from) == false &&*/ StringUtil.isEmpty(to) == false
-		  && StringUtil.isEmpty(message) == false) {
-		try {
-
-		  Message gmailMessage = new MimeMessage(session);
-
-		  // Setting sender
-//		  gmailMessage.setFrom(new InternetAddress(from));
-
-		  // Setting receipients
-		  // Setting to
-		  gmailMessage.setRecipients(Message.RecipientType.TO,
-			  InternetAddress.parse(to));
-		  // Setting cc
-		  if (StringUtil.isEmpty(cc) == false) {
-			gmailMessage.setRecipients(Message.RecipientType.CC,
-				InternetAddress.parse(cc));
-		  }
-		  // Setting bcc
-		  if (StringUtil.isEmpty(bcc) == false) {
-			gmailMessage.setRecipients(Message.RecipientType.BCC,
-				InternetAddress.parse(bcc));
-		  }
-
-		  // Setting subject
-		  if (StringUtil.isEmpty(subject) == false) {
-			gmailMessage.setSubject(subject);
-		  }
-
-		  // Setting message content
-		  // Creating multipart message content
-		  Multipart multipart = new MimeMultipart();
-
-		  // Creating the message part
-		  BodyPart messageBodyPart = new MimeBodyPart();
-		  // Filling the input message
-		  messageBodyPart.setText(message);
-
-		  // Setting text message part to multipart content
-		  multipart.addBodyPart(messageBodyPart);
-
-		  // Creating the attachment part
-		  if (StringUtil.isEmpty(attachmentFilename) == false) {
-			messageBodyPart = new MimeBodyPart();
-			DataSource source = new FileDataSource(attachmentFilename);
-			messageBodyPart.setDataHandler(new DataHandler(source));
-			messageBodyPart.setFileName(attachmentFilename);
-			// Setting attachment part to multipart content
-			multipart.addBodyPart(messageBodyPart);
-		  }
-
-		  // Setting multipart message content
-		  gmailMessage.setContent(multipart);
-
-		  // Sending message
-		  Transport.send(gmailMessage);
-
-		  System.out.println("Mail sent");
-		  return true;
+			else {
+				valid = validateEmails(to);
+			}
 		}
-		catch (MessagingException e) {
-		  System.out.println("Mail not sent: " + e.getMessage());
-		  e.printStackTrace();
+
+		if (StringUtil.isEmpty(cc) == false) {
+			if (cc.contains(",")) {
+				String[] emails = cc.split(",");
+				valid &= validateEmails(emails);
+			}
+			else {
+				valid &= validateEmails(cc);
+			}
 		}
-	  }
+
+		if (StringUtil.isEmpty(bcc) == false) {
+			if (bcc.contains(",")) {
+				String[] emails = bcc.split(",");
+				valid &= validateEmails(emails);
+			}
+			else {
+				valid &= validateEmails(bcc);
+			}
+		}
+
+		return valid;
 	}
-	return false;
-  }
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.iq.comm.Communicator#processCommunication(java.lang.String,
+	 * java.lang.String, java.lang.String, java.lang.String, java.lang.String,
+	 * java.lang.String, java.lang.String)
+	 */
+	@Override
+	protected boolean processCommunication(String from, String to, String cc,
+			String bcc, String subject, String message, String attachmentFilename) {
+
+		if (StringUtil.isEmpty(username) == false
+				&& StringUtil.isEmpty(password) == false) {
+			Properties props = new Properties();
+			props.put(SMTP_AUTH_PARAM_NAME, smtpAuthFlag);
+			props.put(SMTP_TLS_ENABLE_PARAM_NAME, smtpTLSEnableFlag);
+			props.put(SMTP_HOST_PARAM_NAME, smtpHost);
+			props.put(SMTP_PORT_PARAM_NAME, smtpPort);
+
+			Session session = Session.getInstance(props,
+					new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(userName, password);
+				}
+			});
+
+			if (/*StringUtil.isEmpty(from) == false &&*/ StringUtil.isEmpty(to) == false
+					&& StringUtil.isEmpty(message) == false) {
+				try {
+
+					Message gmailMessage = new MimeMessage(session);
+
+					// Setting sender
+					//		  gmailMessage.setFrom(new InternetAddress(from));
+
+					// Setting receipients
+					// Setting to
+					gmailMessage.setRecipients(Message.RecipientType.TO,
+							InternetAddress.parse(to));
+					// Setting cc
+					if (StringUtil.isEmpty(cc) == false) {
+						gmailMessage.setRecipients(Message.RecipientType.CC,
+								InternetAddress.parse(cc));
+					}
+					// Setting bcc
+					if (StringUtil.isEmpty(bcc) == false) {
+						gmailMessage.setRecipients(Message.RecipientType.BCC,
+								InternetAddress.parse(bcc));
+					}
+
+					// Setting subject
+					if (StringUtil.isEmpty(subject) == false) {
+						gmailMessage.setSubject(subject);
+					}
+
+					// Setting message content
+					// Creating multipart message content
+					Multipart multipart = new MimeMultipart();
+
+					// Creating the message part
+					BodyPart messageBodyPart = new MimeBodyPart();
+					// Filling the input message
+					messageBodyPart.setText(message);
+
+					// Setting text message part to multipart content
+					multipart.addBodyPart(messageBodyPart);
+
+					// Creating the attachment part
+					if (StringUtil.isEmpty(attachmentFilename) == false) {
+						messageBodyPart = new MimeBodyPart();
+						DataSource source = new FileDataSource(attachmentFilename);
+						messageBodyPart.setDataHandler(new DataHandler(source));
+						messageBodyPart.setFileName(attachmentFilename);
+						// Setting attachment part to multipart content
+						multipart.addBodyPart(messageBodyPart);
+					}
+
+					// Setting multipart message content
+					gmailMessage.setContent(multipart);
+
+					// Sending message
+					Transport.send(gmailMessage);
+
+					System.out.println("Mail sent");
+					return true;
+				}
+				catch (MessagingException e) {
+					System.out.println("Mail not sent: " + e.getMessage());
+					e.printStackTrace();
+				}
+			}
+		}
+		return false;
+	}
 }
