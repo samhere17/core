a82262975b9c1e33e5630779fd3f305ce6c98161
Tapas Bose <tapas.23571113@gmail.com>
2016-01-30 21:28:35 +0530

    Modify CacheEvictor to use Executor Service instead of Thread

--- src/org/iq/cache/CacheEvictor.java
+++ src/org/iq/cache/CacheEvictor.java
@@ -1,202 +1,37 @@
 package org.iq.cache;
 
-import java.util.Iterator;
-import java.util.Set;
-
-import org.iq.exception.CacheException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Sam
+ * @author Tapas
  * 
  */
-public class CacheEvictor implements Runnable {
+public enum CacheEvictor {
+	INSTANCE;
 
-	private static final int DEFAULT_INTERVAL = 5;
-	private static final int MINUTE_TO_MILLIS = 1000 * 60;
-
-	private static CacheEvictor evictorInstance;
-	private Thread runner;
-	private boolean flag;
-
-	/**
-	 * 
-	 */
+	private final ScheduledExecutorService cacheEvictorService;
+	private final CacheEvictorWorker cacheEvictorWorker;
+	
 	private CacheEvictor() {
-		runner = new Thread(this, "Cache Evictor Thread");
-		runner.setDaemon(true);
+		cacheEvictorService = Executors.newSingleThreadScheduledExecutor();
+		cacheEvictorWorker = new CacheEvictorWorker();
 	}
 
-	/**
-	 * @return CacheEvictor
-	 */
 	public static CacheEvictor getEvictorInstance() {
-		if (evictorInstance == null) {
-			evictorInstance = new CacheEvictor();
-		}
-		return evictorInstance;
+		return CacheEvictor.INSTANCE;
 	}
 
-	/**
-	 * 
-	 */
-	public void startCacheEvictor() {
-		flag = true;
-		runner.start();
+	public void startCacheEvictor() {		
+		cacheEvictorService.scheduleAtFixedRate(cacheEvictorWorker, 5, 5, TimeUnit.MINUTES);
 	}
 
-	/**
-	 * 
-	 */
-	public void stopCacheEvictor() {
-		flag = false;
-	}
+	public void stopCacheEvictor() {		
+		cacheEvictorWorker.stopWork();
+		cacheEvictorService.shutdown();
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Runnable#run()
-	 */
-	public void run() {
-
-		// setting default interval value.
-		int intervalMin = DEFAULT_INTERVAL;
-
-		while (flag == true) {
-			try {
-				System.out.println("[" + runner.getName() + "] "
-						+ "Sleeping for " + intervalMin + " minutes...");
-				Thread.sleep(intervalMin * MINUTE_TO_MILLIS);
-				System.out.println("[" + runner.getName() + "] " + "Sleep for "
-						+ intervalMin + " minutes over.");
-			} catch (InterruptedException e) {
-				System.err.println(e.getMessage());
-			}
-
-			Cache cache = Cache.getInstance();
-			Set<String> regionNamesSet = cache.keySet();
-
-			System.out.println("Cache has got " + regionNamesSet.size()
-					+ " regions.");
-
-			Iterator<String> regionNamesIterator = regionNamesSet.iterator();
-			while (regionNamesIterator.hasNext()) {
-				String regionName = regionNamesIterator.next();
-				CacheRegion cacheRegion = cache.get(regionName);
-				if (cacheRegion.evictionAllowed) {
-					EvictionMarker evictionMarker = new EvictionMarker(
-							regionName);
-					evictionMarker.start();
-				}
-			}
-		}
-	}
-
-	/**
-	 * @author Sam
-	 * 
-	 */
-	private class EvictionMarker extends Thread {
-
-		private String regionName;
-
-		/**
-		 * @param cacheRegion
-		 */
-		private EvictionMarker(String regionName) {
-			this.regionName = regionName;
-			this.setName(regionName + " Eviction Marker Thread");
-			this.setDaemon(true);
-		}
-
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see java.lang.Thread#run()
-		 */
-		public void run() {
-
-			int intervalMin = DEFAULT_INTERVAL;
-			int markedCount = 0;
-
-			Cache cache = Cache.getInstance();
-			CacheRegion cacheRegion = cache.get(regionName);
-			Set<String> elementKeysSet = cacheRegion.keySet();
-			Iterator<String> elementKeysIterator = elementKeysSet.iterator();
-			while (elementKeysIterator.hasNext()) {
-				String currKey = elementKeysIterator.next();
-				CacheElement cacheElement = cacheRegion.get(currKey);
-
-				long lastAccessTime = cacheElement.getLastAccessTime()
-						.getTime();
-				long presentTime = System.currentTimeMillis();
-
-				if ((presentTime - lastAccessTime) > intervalMin
-						* MINUTE_TO_MILLIS) {
-					cacheElement.setCleanable(true);
-					markedCount++;
-				}
-			}
-
-			if (markedCount > 0) {
-				System.out.println("Marked " + markedCount + " elements in "
-						+ regionName + " region.");
-				startCleanerThread(regionName);
-			}
-		}
-	}
-
-	/**
-	 * @author Sam
-	 * 
-	 */
-	private class CleanerThread extends Thread {
-
-		private String regionName;
-
-		/**
-		 * 
-		 */
-		public CleanerThread(String regionName) {
-			this.regionName = regionName;
-			this.setName(regionName + " Cleaner Thread");
-			this.setDaemon(true);
-		}
-
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see java.lang.Thread#run()
-		 */
-		@Override
-		public void run() {
-			Cache cache = Cache.getInstance();
-			CacheRegion cacheRegion = cache.get(regionName);
-			Set<String> elementKeysSet = cacheRegion.keySet();
-			Iterator<String> elementKeysIterator = elementKeysSet.iterator();
-			while (elementKeysIterator.hasNext()) {
-				String currKey = elementKeysIterator.next();
-				CacheElement cacheElement = cacheRegion.get(currKey);
-				if (cacheElement.isCleanable()) {
-					CacheHelper cacheHelper = new CacheHelper();
-					try {
-						cacheHelper.removeElement(regionName, currKey);
-					} catch (CacheException e) {
-						System.err.println(e.getMessage());
-					}
-					System.out.println("Evicted an element : Region = "
-							+ regionName + ", Key = " + currKey
-							+ ", Last Access = "
-							+ cacheElement.getLastAccessTime());
-				}
-			}
-		}
-	}
-
-	/**
-	 * 
-	 */
-	private void startCleanerThread(String regionName) {
-		CleanerThread cleanerThread = new CleanerThread(regionName);
-		cleanerThread.start();
+		while (!cacheEvictorService.isTerminated()) {}
 	}
 }
