0eaf5a06248d958292b092a8fba42339951a2ab5
Sambuddha Chatterjee <sam@iquesters.com>
2016-04-26 21:55:54 +0530

    Update core to add footer

--- web/cmn/tail.jsp
+++ web/cmn/tail.jsp
@@ -1,6 +1,17 @@
 			</main>
 		</div>
 	</div>
+
+	<footer class="container-fluid">
+		<div class="row">
+			<div id="status-bar" class="col-xs-12 col-sm-6 small"></div>
+			<div id="copyright-strip" class="col-xs-12 col-sm-6 small">
+				Copyright&nbsp;&copy;&nbsp;2016&nbsp;&nbsp;<a href="">iquesters</a>
+			</div>
+		</div>
+	</footer>
+
 	<%@include file="js.jsp" %>
+
 	</body>
 </html>
--- web/js/height-and-margin.js
+++ web/js/height-and-margin.js
@@ -1,8 +1,15 @@
 $(window).on("load", function() {
-	var heightOfHeader = $("header").height()
-
+	var heightOfHeader = $("header").innerHeight()
+	var heightOfFooter = $("footer").innerHeight()
+	var height = $(window).height() - heightOfHeader - heightOfFooter
+	
+	/* Setting the margin */
 	$("main").css("margin-top", heightOfHeader)
 	$("#index-main").css("margin-top", heightOfHeader)
 
 	$("aside").css("margin-top", heightOfHeader)
+	
+	/* Setting the margin */
+	$("main").css("height", height)
+	$("aside").css("height", height)
 })
